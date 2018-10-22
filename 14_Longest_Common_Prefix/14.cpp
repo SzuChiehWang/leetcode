d11ac6a404754b35caf581c8ac08843044c1f3eb
SzuChiehWang <scwang1123@gmail.com>
2018-10-22 22:53:46 +0800

    14_Longest_Common_Prefix

---
+++ 14_Longest_Common_Prefix/14.cpp
@@ -0,0 +1,34 @@
+#include<string>
+#include<vector>
+#include<iostream>
+using namespace std;
+
+
+class Solution
+{
+  public:
+	  string longestCommonPrefix(vector<string>&strs){	  
+	    if(strs.size()==0)
+	      return "";
+	    
+ 	    for(int i=0;i<strs[0].size();i++)
+	    {
+	      for(int j=1; j<strs.size();j++)
+              {
+	        if(strs[j].size()<=i||strs[j][i]!=strs[0][i])
+                  return strs[0].substr(0,i);
+              } 
+            
+   	    }
+            return strs[0];
+
+	  }
+};
+
+int main()
+{
+  Solution sol;
+  vector<string> a={"car", "dog", "racecar"};
+  cout<<sol.longestCommonPrefix(a)<<endl;
+  return 0;
+}
